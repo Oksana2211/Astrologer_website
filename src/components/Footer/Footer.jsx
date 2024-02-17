fca2a6c41f3fc9fcf2483fe4e739d0b90f787279
Oksana Taysumova <otajsumova@gmail.com>
2024-02-17 20:21:17 +0300

    correction style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react"
-// import style from "./style.module.scss"
+import style from "./style.module.scss"
+import vector from "../../img/Vector.png"
 
 export default function Footer() {
     return (
-        <section >
-
-        </section>
+        <footer className={style.footer}>
+            <img className={style.footer__vector} src={vector} alt="вектор" />
+            <p className={style.footer__contacts}>Контактная информация</p>
+        </footer>
     )
 }
